98a6ef46d4451891092e3b1dd2032d6a7b089e28
maidousj <maidousj@163.com>
2017-06-27 00:04:46 +0800

    add a test blog

--- _posts/2016-01-23-indigo-jekyll-theme.markdown
+++ _posts/2016-01-23-indigo-jekyll-theme.markdown
@@ -4,12 +4,12 @@ layout: post
 date: 2016-01-23 22:10
 tag: jekyll
 image: https://koppl.in/indigo/assets/images/jekyll-logo-light-solid.png
-headerImage: true
+headerImage: false
 projects: true
-hidden: true # don't count this post in blog pagination
+hidden: false # don't count this post in blog pagination
 description: "This is a simple and minimalist template for Jekyll for those who likes to eat noodles."
 category: project
-author: johndoe
+author: Sun
 externalLink: false
 ---
 
---
+++ _posts/title.py
@@ -0,0 +1,55 @@
+import subprocess
+import sys
+import getopt
+import ipdb
+
+def usage():
+    print "python title.py -t $title -i $image(stored in ../assets/images) -tag $tag"
+
+
+opts, args = getopt.getopt(sys.argv[1:], "ht:i:c:", ["help", "title=", "image=", "tag=", "category="])
+print opts, args
+title=""
+image=""
+tag=""
+category="blog"
+
+for op, value in opts:
+    if op in ("-t", "--title"):
+        title = value
+    if op in ("-i", "--image"):
+        image = value
+    if op in ("--tag"):
+        tag = "- "+value+"\n"
+        for t in args:
+            tag = tag + "- " +t+"\n"
+    if op in ("-c", "--category"):
+        category = value
+    if op in ("-h", "--help"):
+        usage()
+        sys.exit()
+
+datetime = subprocess.check_output('date \"+%Y-%m-%d %H:%M\"', shell=True)
+date = subprocess.check_output('date \"+%Y-%m-%d\"', shell=True)
+date = date.replace('\n', '-')
+
+if title == "":
+    print "You must input title at least"
+    sys.exit(1)
+
+titlecontent=["---",
+"\ntitle: "+ title,
+"\nlayout: post",
+"\ndate: "+ datetime,
+"image: /assets/images/"+ image,
+"\ncategory: "+ category,
+"\ntag:\n"+ tag,
+"author: Sun",
+"\n---"]
+
+f = open(date+title+".md", 'w')
+for content in titlecontent:
+    f.writelines(content)
+    print content
+
+f.close()
